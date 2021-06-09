08df832a5d2d2ff1a9a5987b1b95ecb542c367ed
unknown <nelljannes@gmail.com>
2021-06-09 13:11:53 +0200

    Improved collection adding screen, added arrays to viewcollecctions for easy data retrieval

--- app/src/main/java/com/example/opsc7311_task2_group2/ViewCollections.kt
+++ app/src/main/java/com/example/opsc7311_task2_group2/ViewCollections.kt
@@ -11,7 +11,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class ViewCollections : AppCompatActivity() {
 
-    private val categories = arrayListOf<String>()
+    private val categoryNames = arrayListOf<String>()
+    private val categoryGoals = arrayListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_collections)
@@ -22,7 +23,8 @@ class ViewCollections : AppCompatActivity() {
             startActivity(newCollectionIntent)
         }
 
-        categories.clear()
+        categoryNames.clear()
+        categoryGoals.clear()
         readCollections()
     }
 
@@ -33,18 +35,22 @@ class ViewCollections : AppCompatActivity() {
             .get()
             .addOnCompleteListener {
 
+                val categories = arrayListOf<String>()
+                categories.clear()
                 if (it.isSuccessful) {
                     for (document in it.result!!) {
 
                         val result = (document.id).toString() +" Goal:"+ (document.data.getValue("Goal")).toString()
                         categories.add(result)
+                        categoryNames.add(document.id)
+                        categoryGoals.add(document.data.getValue("Goal").toString())
                     }
                     val listView = findViewById<ListView>(R.id.listCollections)
                     val arrayAdapter : ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, categories )
                     listView.adapter = arrayAdapter
 
                     listView.setOnItemClickListener { adapterView, view, i, l ->
-                        Toast.makeText(this, "Category Selected "+categories[i], Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Category Selected "+categoryNames[i], Toast.LENGTH_SHORT).show()
                     }
                 }
             }
